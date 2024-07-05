84129315f398777fa5590f6131d223b54834ca05
Marc McAllister <129139183+markiskorova@users.noreply.github.com>
2024-07-05 15:12:55 -0700

    Add files via upload

---
+++ Code/CatalogAPI/CatalogAPI/CatalogAPI/CatalogModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CatalogAPI
+{
+    public class CatalogModel
+    {
+        public Int32 Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public CatalogModel() { }
+
+    }
+}
---
+++ Code/CatalogAPI/CatalogAPI/CatalogAPI/Data/Entities/Episode.cs
@@ -0,0 +1,17 @@
+﻿namespace CatalogAPI.Data.Entities
+{
+    public class Episode
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public int Season { get; set; }
+
+        public int EpisodeNumber { get; set; }
+        public DateTime AirDate { get; set; }   
+
+        
+        public Episode() { }
+
+    }
+}
